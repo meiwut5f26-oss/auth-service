7cfe651301160eb889736c74f1951b3f202d4d80
Nguyễn Võ Hoàng Long <longxautrai12@gmail.com>
2026-03-25 18:26:15 +0700

    [ADD]VALIDATION FOR REQUEST

---
+++ src/main/java/service/CSFC/CSFC_auth_service/common/config/MailConfig.java
@@ -0,0 +1,37 @@
+package service.CSFC.CSFC_auth_service.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(
+            @Value("${mail.host:smtp.gmail.com}") String host,
+            @Value("${mail.port:587}") int port,
+            @Value("${mail.username}") String username,
+            @Value("${mail.password}") String password) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.trust", host);
+        props.put("mail.debug", "false");
+
+        return mailSender;
+    }
+}
+
